a9ca872b39734ee7742422466b6b31b0e33306bb
droy10 <53913716+droy10@users.noreply.github.com>
2019-10-21 17:13:58 -0400

    Add files via upload
    These 2 files, ui.R and server.R, are for the first part of the final assignment of the Data Products Coursera course

---
+++ ui.R
@@ -0,0 +1,19 @@
+library(shiny)
+
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("Horsepower Confidence Interval"),
+
+    sidebarLayout(
+        sidebarPanel(
+            selectInput("ConfLevel", "Confidence Interval", choices=c(80, 90, 95, 99)),
+            submitButton("Submit")
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            plotOutput("plot1"),
+        )
+    )
+))
